18946b680d35cbfa943c7a2ce90c67182a0738d9
Angel Flores <angel-abraham.flores.7e8@itb.cat>
2025-10-16 12:48:39 +0200

    SumTwoNumbers

---
+++ damv-bloc1-ac06-implementacio/damv-bloc1-ac06-implementacio/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        // PRECONDITION: The user is prompted to input two integers.
+        const string Msg_Input1 = "Enter the first integer: ",
+            Msg_Input2 = "Enter the second integer: ",
+            Msg_Result = "The sum is: ";
+        int num1, num2, sum;
+        Console.WriteLine(Msg_Input1);
+        num1 = Int32.Parse(Console.ReadLine());
+        Console.WriteLine(Msg_Input2);
+        num2 = Int32.Parse(Console.ReadLine());
+        sum = num1+ num2;
+        Console.WriteLine(Msg_Result + sum);
+        // POSTCONDITION: 'sum' contains the sum of 'num1' and 'num2', and it is displayed.
+    }
+}
